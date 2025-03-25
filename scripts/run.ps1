d60ef201f6f46029d6e2fe32ae381b7dc4cd1425
Roman Osadsky <rmnosadsky@gmail.com>
2025-03-25 13:04:25 +0100

    Add mongodb compose file

---
+++ scripts/run.ps1
@@ -0,0 +1,28 @@
+...
+$env:AMBULANCE_API_PORT="8080"
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
+
+switch ($command) {
+    "openapi" {
+        docker run --rm -ti  -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    "start" {
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/ambulance-api-service
+        } finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
+    }
+    default {
+        throw "Unknown command: $command"
+    }
+}
